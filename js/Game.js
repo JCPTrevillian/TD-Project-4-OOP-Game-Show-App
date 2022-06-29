7215d190cbab5066d85e380d7e55348ba891c7db
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-06-29 14:34:00 -0700

    initial commit

--- js/Game.js
+++ js/Game.js
@@ -18,7 +18,6 @@ class Game {
        this.activePhrase.addPhraseToDisplay();
    }
 
- 
    getRandomPhrase(){
        const randNum = Math.floor(Math.random() * this.phrases.length);
        return this.phrases[randNum];
@@ -55,3 +54,76 @@ class Game {
            }
        }
    }
+ //removes color from 1 heart for each incorrect letter  
+  removeLife(){
+    const lifes = document.querySelectorAll('#scoreboard .tries img[src*="liveHeart"]');
+    if(lifes.length){
+        lifes[lifes.length - 1].src = 'images/lostHeart.png';
+    }
+    this.missed++;
+}
+
+//checks letters for win/lose 
+checkForWin(){
+    const list = document.querySelector('#phrase ul').children;
+    for(let i = 0; i < list.length; i++) {
+        if(list[i].classList.contains('hide')){
+            if(this.missed > 4) {
+                this.gameOver(false);
+            }
+            return;
+        }
+    }
+    // game won?
+    this.gameOver(true);
+}
+
+//overlay - game over 
+gameOver(state){
+    this.missed = 0;
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = '';
+    const gameOverMessage = document.getElementById('game-over-message');
+    window.removeEventListener('keypress', game.handleInteraction);
+
+    //overlay win/lose
+    if(state){
+        gameOverMessage.textContent = 'You won!';
+        overlay.className = 'won';
+    } else {
+        gameOverMessage.textContent = 'You lost!';
+        overlay.className = 'lost';
+    }
+
+    // reset keyboard 
+    const qwerty = document.querySelectorAll('#qwerty button');
+    document.querySelector('#phrase ul').innerHTML = '';
+    for(let i = 0; i < qwerty.length; i++) {
+        if(qwerty[i].classList.contains('disabled')){
+            qwerty[i].disabled = false;
+            qwerty[i].classList.remove('disabled');
+        }
+    }
+
+    // reset heart 
+    const lifes = document.querySelectorAll('#scoreboard .tries img[src*="lostHeart"]');
+    for(let i = 0; i < lifes.length; i++){
+        lifes[i].src = 'images/liveHeart.png';
+    }
+}
+//create phrases - pull phrases from the list of 5 options 
+ createPhrases() {
+    const phrases = [
+        new Phrase('A foot in the door'),
+        new Phrase('Drop dead gorgeous'),
+        new Phrase('End of story'),
+        new Phrase('Easy as pie'),
+        new Phrase('All in all')
+    ];
+    return phrases;
+};
+}
+
+
+
+
