1e2a6c6d238cec154bb092e2908d880cd5e4535a
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-06-29 00:17:17 -0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//game setup 
+const game = new Game();
+const startButton = document.querySelector('#btn__reset');
+const keyboard = document.getElementById('qwerty');
+
+// event listener initiates game initialization 
+startButton.addEventListener('click', e => {
+    game.startGame();
+    keyboard.addEventListener('click', game.handleInteraction);
+    window.addEventListener('keypress', game.handleInteraction);
+});
