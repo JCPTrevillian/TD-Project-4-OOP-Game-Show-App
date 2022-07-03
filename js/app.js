626d47951e8a72f2a9ac244678a8f49f5576b049
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-07-03 15:34:03 -0700

    correction line 6, 12

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
  * app.js */
 
 //game setup 
-let game = new Game();
 let startButton = document.querySelector('#btn__reset');
 let keyboard = document.getElementById('qwerty');
 
 // event listener initiates game initialization 
 startButton.addEventListener('click', e => {
+    let game = newGame ();
     game.startGame();
     keyboard.addEventListener('click', game.handleInteraction);
     window.addEventListener('keypress', game.handleInteraction);
